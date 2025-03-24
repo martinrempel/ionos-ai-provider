23b62f3ea7ba764a6f7493b4b3e3a89cccae8b9d
Martin Rempel <46716374+martinrempel@users.noreply.github.com>
2025-03-24 21:12:19 +0100

    fix: build command

--- src/ionos-embedding-settings.ts
+++ src/ionos-embedding-settings.ts
@@ -1,9 +1,11 @@
 import { OpenAICompatibleEmbeddingSettings } from "@ai-sdk/openai-compatible";
 
+// https://docs.ionos.com/cloud/ai/ai-model-hub/tutorials/text-embeddings
 export type IonosEmbeddingModelId =
   | "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
   | "BAAI/bge-large-en-v1.5"
   | "BAAI/bge-m3"
   | (string & {});
 
-export interface IonosEmbeddingSettings extends OpenAICompatibleEmbeddingSettings { }
+export interface IonosEmbeddingSettings
+  extends OpenAICompatibleEmbeddingSettings {}
